a92dde34c81232b4c7955ed0993abfffa57c64c4
Abhay1Dhakl <102395633+Abhay1Dhakl@users.noreply.github.com>
2023-04-07 19:04:53 +0545

    Updates

---
+++ precache-manifest.dabed5c4acab34e3f7f7e327fe51a84b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "816d411df81a93f7c13c",
+    "url": "/React-TourWebsite/static/css/main.5812ecbc.chunk.css"
+  },
+  {
+    "revision": "816d411df81a93f7c13c",
+    "url": "/React-TourWebsite/static/js/main.816d411d.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/React-TourWebsite/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/React-TourWebsite/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "8c5d1342dfaf26aacbb5",
+    "url": "/React-TourWebsite/static/js/runtime~main.8c5d1342.js"
+  },
+  {
+    "revision": "967f4c494458462a8aa015919329849b",
+    "url": "/React-TourWebsite/index.html"
+  }
+];
